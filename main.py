df087de324dec4e64be105dc1fa8fa2e4ea957ba
Mitrano-sensei <56512461+Mitrano-sensei@users.noreply.github.com>
2022-09-02 17:35:43 +0200

    Analyseur syntaxique

--- main.py
+++ main.py
@@ -7,10 +7,23 @@ class Token():
         self.position = position
 
     def __repr__(self):
-        print(f"Type : {self.type}, Valeur : {self.valeur}, Position : {self.position}")
+        return (f"Type : {self.type}, Valeur : {self.valeur}, Ligne : {self.position}")
 
+class Node:
+    def __init__(self, type, valeur, ligne, children = None):
+        self.type = type
+        self.valeur = valeur
+        self.ligne = ligne
+        if children == None:
+            self.children = []
+        else:
+            self.children = children
 
-# MAIN
+    
+    def add_child(self, child):
+        self.children.append(child)
+
+# Le compilo
 ## Lecture de fichier
 src = sys.argv[1]
 dest = sys.argv[2]
@@ -21,11 +34,23 @@ with open(src) as file:
     string = file.readlines()
     string = "".join(string)
 
+## Analyse linguistique
 def next():
     global string
     global position
     global ligne
 
+    global courant
+    global precedant
+
+    if position >= len(string):
+        token = Token("EOS", None, ligne)
+        precedant = courant
+        courant = token
+
+        print(courant)
+        return
+    
     char =  string[position]
     token = None
 
@@ -36,7 +61,8 @@ def next():
     elif char == ' ' or char == '\t' or char == '\r':
         position+=1
         return next()
-    elif char == '(':
+    
+    if char == '(':
         position += 1
         token = Token("po", None, ligne)
     elif char == ')':
@@ -70,8 +96,12 @@ def next():
         position += 1
         token = Token("mod", None, ligne)
     elif char == '&':
-        ## TODO
-        pass
+        if string[position+1] == '&':
+            position+=2
+            token = Token("and", None, ligne)
+        else:
+            position+=1
+            token = Token("esper", None, ligne)
     elif char == '>':
         ## TODO
         pass
@@ -79,14 +109,31 @@ def next():
         ## TODO
         pass
     elif char == '!':
-        ## TODO
-        pass
+        position+=1
+        token = Token("neglog", None, ligne)
+
+        if position < len(string):
+            if string[position] == "=":
+                position+=1
+                token = Token("notequal", None, ligne)
     elif char == '=':
-        ## TODO
-        pass
+        position+=1
+        token = Token("assign", None, ligne)
+        if position < len(string):
+            if string[position]:
+                position+=1
+                token = Token("equal", None, ligne)
     elif char == '|':
-        ## (un seul = erreur) TODO
-        pass
+        position+=1
+        if position < len(string):
+            if string[position] == '|':
+                position+=1
+                token = Token("or", None, ligne)
+            else:
+                error()
+        else:
+            error()
+            
     elif char == ';':
         position += 1
         token = Token("pvirg", None, ligne)
@@ -96,51 +143,158 @@ def next():
     else:
         # MOTS CLES
         if ord('z') >= ord(char.lower()) >= ord('a'):
-            if char == 'i' and string[position+1] == 'f':
-                position+=2
-                token = Token("if", None, ligne)
-            elif char == 'r':
-                # Verif return TODO : FAIRE UN SLICE, JSP COMMENT ON FAIT
-                pass
-            elif char == 'e':
-                # Verif else TODO
-                pass
-            else:
-                s = "" + char
-                position+=1
+            s = "" + char
+            position+=1
+            if position < len(string):
                 char = string[position]
                 while ord('z') >= ord(char.lower())  >= ord('a'):
                     s+= char
                     position+=1
-                    char=string[position]
+                    if position < len(string):
+                        char=string[position]
+                    else:
+                        break
+                if s == "return":
+                    token = Token("return", None, ligne)
+                elif s == "if":
+                    token = Token("if", None, ligne)
+                elif s == "else":
+                    token = Token("else", None, ligne)
+                elif s == "for":
+                    token = Token("for", None, ligne)
+                elif s == "while":
+                    token = Token("while", None, ligne)
+                elif s == "do":
+                    token = Token("do", None, ligne)
+                elif s == "int":
+                    token = Token("int", None, ligne)
+                elif s == "break":
+                    token = Token("break", None, ligne)
+                elif s == "continue":
+                    token = Token("continue", None, ligne)
+                else:
+                    token = Token("var", s, ligne)           
+            else:
+                token = Token("var", s, ligne)
                 
-                token = Token("str", s, ligne)
-            pass
         elif char.isdigit():
             s = "" + char
             position+=1
-            char = string[position]
-            while ord('z') >= ord(char.lower())  >= ord('a'):
-                s+= char
-                position+=1
-                char=string[position]
-            token = Token("const", s, ligne) # TODO : Transformer s en nombre
-    # TODO : End Of Stream
-
-
+            if position < len(string):
+                char = string[position]
+                while char.isdigit():
+                    s+= char
+                    position+=1
+                    if position < len(string):
+                        char=string[position]
+                    else:
+                        break
+                token = Token("const", s, ligne)
+            else:
+                token = Token("const", s, None)
+    
     ## Verif Erreurs
     if token == None:
-        return -1
-    
-    ## Return
-    return token
+        error()
 
-## Tings
+    ## Return
+    precedant = courant
+    courant = token
+
+    print(courant)
+
+## Analyse Synthaxique
+
+def G():
+    return F()
+
+def F():
+    return I()
+
+def I(): 
+    return E()
+
+def E():
+    return P()
+
+def P():
+    if check('sub'): 
+        N = P()
+        M = Node('sub', None, courant.position)
+        M.add_child(N)
+        return M
+    elif check('add'):
+        return P()
+    elif check('neglog'):
+        N = P()
+        M = Node('neglog', None, courant.position)
+        M.add_child(N)
+        return M
+    else:
+        return S()
+
+def S():
+    return A()
+
+def A():
+    if check("const"):
+        return Node('const', precedant.valeur, precedant.position) 
+    elif check("po"):
+        N = E()
+        accept('pf')
+        return N
+    else:
+        error()
+
+def error():
+    # TODO
+    # Faire une fonction erreur
+    print("ERROR OMG OMG OMG !!!")
+
+def check(type):
+    global courant
+    global precedant
+
+    if (courant.type == type):
+        next()
+        return True
+    else:
+        return False
+
+def accept(type):
+    if not check(type):
+        error()
+
+def AS():
+    # G (le plus général, englobe les fonctions et plein de trucs qu'on utilise pas) = F *  (L'étoile signifie qu'on peut en avoir autant qu'on veut, mais on l'ignore pour l'instant)
+    # F (une fonction) = I
+    # I (une instruction) = E
+    # E (une expression) =  P + E | P
+    # P (pour prefixe)= -P | +P | !P | S
+    # S (pour suffixe)= A
+    # A (pour Atome) = Constante | ( E )
+    return None
+
+## Analyse Sementique
+def ASe():
+    return AS()
+
+## Generation de Code
+def Gc():
+    Node = ASe()
+    # TODO
+    # Ecrire un fichier qui commence par un label .start puis les instructions assembleurs
+    # Le premier :
+    ##  .start
+    ##      halt
+    pass
+
+
+# Main
 courant = None
 precedant = None
 
-n = next()
-while n.type != "EOS":
-    precedant = courant
-    courant = n
-    print(courant)
+next()
+while courant.type != "EOS":
+    print()
+    next()
